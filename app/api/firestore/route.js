32ec261ab97417764a39377ca13314aed6fc32d9
Juan Felipe Cano <juan.canon@upb.edu.co>
2025-05-15 19:32:59 -0500

    Innecesary Folder Elimination

--- app/api/firestore/route.js
+++ app/api/firestore/route.js
@@ -1,30 +1,27 @@
 import { getFirestore, collection, getDocs, doc, setDoc } from "firebase/firestore";
-import { db, auth } from '../../../firebase';
+import { db } from '../../../firebase/firebase';
 import { NextResponse } from "next/server";
 
-const firestore = getFirestore(db);
-
 export async function GET() {
-    const snapshot = await getDocs(collection(firestore, "cartas"));
-
-    const data = snapshot.docs.map((doc) => ({
-        ...doc.data(),
-    }));
-
-    return NextResponse.json(data);
+    try {
+        const snapshot = await getDocs(collection(db, "cartas"));
+        const data = snapshot.docs.map((doc) => ({
+            ...doc.data(),
+        }));
+        return NextResponse.json(data);
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to fetch documents: " + error.message }, { status: 500 });
+    }
 }
 
 export async function POST(req) {
-    const { data } = await req.json();
-
     try {
-        const docRef = doc(collection(firestore, "cartas"));
+        const { data } = await req.json();
+        const docRef = doc(collection(db, "cartas"));
         const id = docRef.id;
         await setDoc(docRef, { ...data, id });
-
         return NextResponse.json({ id: id, ...data });
     } catch (error) {
-
-        return NextResponse.json({ error: "Failed to add document " + error });
+        return NextResponse.json({ error: "Failed to add document: " + error.message }, { status: 500 });
     }
 }
